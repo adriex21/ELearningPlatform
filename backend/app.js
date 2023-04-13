3ff6fa19f3e83de56747c67627ca7861f4a0d43d
Alex <adytzu.iulian14@gmail.com>
2023-04-13 19:49:28 +0300

    succesfully compiling c++ code

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,32 @@ app.use(cors());
 app.options('*', cors())
 app.listen(port, console.log('Server is on port: ' + port));
 
-
+app.post('/compile', async (req, res) => {
+    const code = req.body.code;
+    console.log(req.body);
+    // Create a Docker container from an image that has your desired compiler
+    const container = await docker.createContainer({
+      Image: 'gcc:latest',
+      Tty: true,
+      Cmd: ['sh', '-c', `echo "${code.replace(/"/g, '\\"')}" > code.cpp && g++ code.cpp -o code && ./code`],
+    });
+  
+    // Start the container
+    await container.start();
+  
+    // Wait for the container to finish running the command
+    await container.wait();
+  
+    // Get the output of the command from the container logs
+    const output = await container.logs({ stdout: true, stderr: true });
+    const outputString = output.toString('utf8').trim();
+  
+    // Remove the container
+    await container.remove();
+  
+    // Send the output back to the client
+    res.send({ output: outputString });
+  });
+  
 
 module.exports = app;
--- frontend/src/Components/CodeEditor.js
+++ frontend/src/Components/CodeEditor.js
@@ -1,21 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AceEditor from 'react-ace';
 import brace from 'brace'
 import axios from 'axios';
-import 'brace/mode/javascript'
-import 'brace/theme/cobalt'
+import 'brace/mode/c_cpp';
+import 'brace/theme/cobalt';
+import 'brace/ext/language_tools';
+
 function CodeEditor() {
   const [code, setCode] = useState('');
   const [output, setOutput] = useState('');
 
-  const handleCodeChange = newCode => {
-    setCode(newCode);
+  const handleCodeChange =  newCode => {
+     setCode(newCode);
+  };
+
+  const handleRunCode = async () => {
+    try {
+      console.log("Code:", code);
+      const response = await axios.post('http://localhost:3000/compile',  { code },{ 
+        headers: {
+        'Content-Type': 'application/json'
+      }});
+      const { output } = response.data;
+      setOutput(output); 
+      console.log(response.data)
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <>
       <AceEditor
-        mode="javascript"
+        mode="c_cpp"
         name="my-ace-editor"
         theme="cobalt"
         value={code}
@@ -31,16 +48,8 @@ function CodeEditor() {
           showLineNumbers: true,
           tabSize: 2,
         }}
-        commands={[
-          {
-            name: 'runCode',
-            bindKey: { win: 'Ctrl-Enter', mac: 'Command-Enter' },
-            exec: editor => {
-              
-            },
-          },
-        ]}
       />
+      <button onClick={handleRunCode}>Run Code</button>
       <div>Output:</div>
       <div>{output}</div>
     </>
